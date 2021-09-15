f433afb003eeeb3d2a4724ce76f961542913fb36
everwithadream <4everwithadream@gmail.com>
2021-09-16 00:27:11 +0300

    Update main.js

--- Lesson4/js/main.js
+++ Lesson4/js/main.js
@@ -2,4 +2,4 @@ let bigText = 'Let its snow \'G\' man to be happy dlfgdfg,dgfdg dsgd\' dgsgdsfgf
 
 console.log('1 задание:',bigText.replace(/'/gm, '"'));
 let b = bigText.replace(/(\s\')/gm, ' "');
-console.log('2 задание:',b.replace(/(\'\s)/gm, '" '))
+console.log('2 задание:',b.replace(/(\'\s)/gm, '" '));
