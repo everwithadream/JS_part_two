d6c64e32c0b9b42b7897d9f8d6d7a1db8fa104ff
everwithadream <4everwithadream@gmail.com>
2021-09-30 00:49:56 +0300

    Lesson 7 add
    some changes

--- Lesson3/js/main.js
+++ Lesson3/js/main.js
@@ -16,24 +16,24 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 //   xhr.send();
 // };
 
-// let getRequest = (url) => {
-//   return new Promise ((resolve, reject) => {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', url, true);
-//     xhr.onreadystatechange = () => {
-//       if (xhr.readyState === 4) {
-//         console.log(xhr.readyState);
-//         if (xhr.status === 200) {
-//           resolve(xhr.responseText);
-//           console.log(xhr.responseText);
-//         } else {
-//           reject(console.log('Error'));
-//         }
-//       }
-//     }
-//   xhr.send();
-//   })
-// };
+let getRequest = (url) => {
+  return new Promise ((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        console.log(xhr.readyState);
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+          console.log(xhr.responseText);
+        } else {
+          reject(console.log('Error'));
+        }
+      }
+    }
+  xhr.send();
+  })
+};
 
 class ProductList {
 
@@ -41,7 +41,7 @@ class ProductList {
     this.container = container;
     this.goods = [];
     this.allProducts = [];
-
+    // this.render();
     this.fetchGoods().then((data) => {
       console.log(data)
       this.goods = data;
@@ -50,10 +50,22 @@ class ProductList {
   }
 
   fetchGoods() {
-    return fetch(`${API}/catalogData.json`)
-        .then(response => response.json())
-        .catch((err) => console.log(err));
-  }
+    getRequest(`${API}/catalogData.json`)
+      .then (
+        response => response => response.json(),
+        error => console.log(err),
+      )
+      // console.log(data);
+      // this.#goods = JSON.parse(data);
+      // this.#render();
+      // // console.log(this.#goods);
+    }
+
+  // fetchGoods() {
+  //   return fetch(`${API}/catalogData.json`)
+  //       .then(response => response.json())
+  //       .catch((err) => console.log(err));
+  // }
 
   sum() {
     return this.allProducts.reduce((sum, { price }) => sum + price, 0);
@@ -91,74 +103,74 @@ class ProductItem {
   }
 }
 
-class Busket {
-  constructor(container = '.busket') {
-    this.container = container;
-    this.goods = [];
-    this.allProducts = [];
-    this.fetchGoods().then((data) => {
-      console.log(data)
-      this.goods = data;
-      this.init();
-    });
-    this.addProduct().then((data) => {
-      console.log(data)
-      this.goods = data;
-    });
-    this.delProduct().then((data) => {
-      console.log(data)
-      this.goods = data;
-    });
-  }
-  fetchGoods() {
-    return fetch(`${API}/getBasket.json`)
-        .then(response => response.json())
-        .catch((err) => console.log(err));
-  }
-  addProduct(element){
-    return fetch(`${API}/addToBasket.json`)
-      .then(response => response.json())
-      .catch((err) => console.log(err));
-  }
-  delProduct(element) {
-    return fetch(`${API}/deleteFromBasket.json`)
-      .then(response => response.json())
-      .catch((err) => console.log(err));
-  }
-  init() {
-    const block = document.querySelector(this.container);
-    for (let product of this.goods) {
-      const productObject = new ItemBusket(product);
-      this.allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render());
-    }
-  }
-}
+// class Busket {
+//   constructor(container = '.busket') {
+//     this.container = container;
+//     this.goods = [];
+//     this.allProducts = [];
+//     this.fetchGoods().then((data) => {
+//       console.log(data)
+//       this.goods = data;
+//       this.init();
+//     });
+//     this.addProduct().then((data) => {
+//       console.log(data)
+//       this.goods = data;
+//     });
+//     this.delProduct().then((data) => {
+//       console.log(data)
+//       this.goods = data;
+//     });
+//   }
+//   fetchGoods() {
+//     return fetch(`${API}/getBasket.json`)
+//         .then(response => response.json())
+//         .catch((err) => console.log(err));
+//   }
+//   addProduct(element){
+//     return fetch(`${API}/addToBasket.json`)
+//       .then(response => response.json())
+//       .catch((err) => console.log(err));
+//   }
+//   delProduct(element) {
+//     return fetch(`${API}/deleteFromBasket.json`)
+//       .then(response => response.json())
+//       .catch((err) => console.log(err));
+//   }
+//   init() {
+//     const block = document.querySelector(this.container);
+//     for (let product of this.goods) {
+//       const productObject = new ItemBusket(product);
+//       this.allProducts.push(productObject);
+//       block.insertAdjacentHTML('beforeend', productObject.render());
+//     }
+//   }
+// }
 
-class ItemBusket {
-  constructor(product, img = 'https://via.placeholder.com/50x100') {
-    this.title = product.product_name;
-    this.price = product.price;
-    this.id = product.id_product;
-    this.quantity = item.quantity;
-  }
-  render(){
-    return `<div class="cart-item" data-id="${this.id_product}">
-            <div class="product-bio">
-            <img src="${this.img}" alt="Some image">
-            <div class="product-desc">
-            <p class="product-title">${this.product_name}</p>
-            <p class="product-quantity">Количество: ${this.quantity}</p>
-        <p class="product-single-price">${this.price} за ед.</p>
-        </div>
-        </div>
-        <div class="right-block">
-            <p class="product-price">${this.quantity*this.price} ₽</p>
-            <button class="del-btn" data-id="${this.id_product}">&times;</button>
-        </div>
-        </div>`
-  }
-}
+// class ItemBusket {
+//   constructor(product, img = 'https://via.placeholder.com/50x100') {
+//     this.title = product.product_name;
+//     this.price = product.price;
+//     this.id = product.id_product;
+//     this.quantity = item.quantity;
+//   }
+//   render(){
+//     return `<div class="cart-item" data-id="${this.id_product}">
+//             <div class="product-bio">
+//             <img src="${this.img}" alt="Some image">
+//             <div class="product-desc">
+//             <p class="product-title">${this.product_name}</p>
+//             <p class="product-quantity">Количество: ${this.quantity}</p>
+//         <p class="product-single-price">${this.price} за ед.</p>
+//         </div>
+//         </div>
+//         <div class="right-block">
+//             <p class="product-price">${this.quantity*this.price} ₽</p>
+//             <button class="del-btn" data-id="${this.id_product}">&times;</button>
+//         </div>
+//         </div>`
+//   }
+// }
 
-let products = new ProductList();
-let busket = new Busket();
+const list = new ProductList();
+// let busket = new Busket();
